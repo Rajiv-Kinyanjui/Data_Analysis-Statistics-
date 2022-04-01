e788faf6b5bdc2cd689ee1e354bd6233c3c86164
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-04-01 10:54:18 +0300

    Plot line of best fit using linear regression

--- data.py
+++ data.py
@@ -16,12 +16,31 @@
 # print(b)
 # print(c)
 
-import numpy
+# import numpy
+# import matplotlib.pyplot as plt
+
+# x = numpy.random.normal(5,1, 1000)
+# y = numpy.random.normal(10,2, 1000)
+
+# #print (x)
+# plt.scatter(x,y)
+# plt.show()
+
+from scipy import stats
 import matplotlib.pyplot as plt
 
-x = numpy.random.normal(5,1, 1000)
-y = numpy.random.normal(10,2, 1000)
+x = [5,7,8,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,11,86,103,87,94,78,77,85]
 
-#print (x)
+# plt.scatter(x,y)
+# plt.show()
+slope, intercept, r, p, std_err = stats.linregress(x,y)
+print(r)
+# print(slope)
+def myfunc(x):
+    return slope*x+intercept
+
+model = list(map(myfunc,x))
 plt.scatter(x,y)
+plt.plot(x,model)
 plt.show()
