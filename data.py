31ac40ad4bfcb343ec31aa5142630551537419b5
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-04-01 10:02:01 +0300

    Generate random numbers from 0-5 using numpy

--- data.py
+++ data.py
@@ -1,17 +1,23 @@
-import numpy
-from scipy import stats 
+# import numpy
+# from scipy import stats 
 
-speed = [80, 70, 50 ,30, 30, 50, 50, 50]
-x = numpy.mean(speed)
-y = numpy.median(speed)
-z = stats.mode(speed)
-a = numpy.std(speed)
-b = numpy.var(speed)
-c = numpy.percentile(speed, 75)
+# speed = [80, 70, 50 ,30, 30, 50, 50, 50]
+# x = numpy.mean(speed)
+# y = numpy.median(speed)
+# z = stats.mode(speed)
+# a = numpy.std(speed)
+# b = numpy.var(speed)
+# c = numpy.percentile(speed, 75)
+
+# print (x)
+# print (y)
+# print (z)
+# print (a)
+# print(b)
+# print(c)
+
+import numpy
+
+x = numpy.random.uniform(0,5, 100)
 
 print (x)
-print (y)
-print (z)
-print (a)
-print(b)
-print(c)
