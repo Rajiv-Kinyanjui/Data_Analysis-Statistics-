42ddcaa93c4f1f6e13d6b90520c7e0f1ec3a22a3
Rajiv-Kinyanjui <peterkinyanjui226@gmail.com>
2022-04-01 09:49:56 +0300

    Add the scipy import with stats to calculate the mode

--- data.py
+++ data.py
@@ -1,7 +1,11 @@
 import numpy
-speed = [80, 70, 50 ,30]
+from scipy import stats 
+
+speed = [80, 70, 50 ,30, 30, 50, 50, 50]
 x = numpy.mean(speed)
 y = numpy.median(speed)
+z = stats.mode(speed)
 
 print(x)
 print(y)
+print(z)
